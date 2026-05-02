a0093ceef263c0cf0649d7b0361f0fcd4262c1fb
evgeny-levkov <evgenlev2004@mail.ru>
2026-05-02 15:05:27 +0300

    start ui second screen

--- Ui/operationWindow.py
+++ Ui/operationWindow.py
@@ -1,29 +1,112 @@
 from PyQt6.QtCore import pyqtSignal
-from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QStackedWidget, QComboBox, QFileDialog
+import pandas as pd
 
-class OperationScreen(QWidget):  
+class OperationScreen(QWidget):
 
     escape = pyqtSignal()
 
     def __init__(self):
         super().__init__()
+        self.stacked_widget = QStackedWidget()
+        self.load_data = QWidget()
+        self.form_report = QWidget()
+        self.file = None
+
+        self.stacked_widget.addWidget(self.load_data)
+        self.stacked_widget.addWidget(self.form_report)
+
         self.initlUi()
 
     def initlUi(self):
-        self.loyuot1 = QVBoxLayout()
+        self.main_loyuot = QVBoxLayout()
+        self.button_loyout = QHBoxLayout()
 
-        self.new = QLabel("Новое окно")
-        self.new.setProperty("class", "label5")
+        self.load_data_button = QPushButton("Загрузка файлов операций\nЗагрузка XLSX-файлов, ETL-валидация, запись в БД")
+        self.load_data_button.setProperty("class", "upload")
+        self.form_report_button = QPushButton("Сформировать отчёт\nАналитика, прогнозы, экспорты PDF, CSV, PNG")
+        self.form_report_button.setProperty("class", "upload")
+        self.load_data_button.clicked.connect(self.show_load_data)
+        self.form_report_button.clicked.connect(self.show_form_report)
+        self.button_loyout.addWidget(self.load_data_button)
+        self.button_loyout.addWidget(self.form_report_button)
 
-        self.loyuot1.addWidget(self.new)
+        self.load_data_loyuot = QVBoxLayout()
+        self.form_report_loyout = QVBoxLayout()
+
+        self.label1 = QLabel("📎 Загрузка файлов операций")
+        self.label1.setProperty("class", "label4")
+
+        self.label2 = QLabel("Выберите тип продукта и приложите XLSX-файл (соответствует Приложению 2 – модель данных)")
+        self.label2.setWordWrap(False)
+        self.label2.setProperty("class", "label4")
+
+        self.label3 = QLabel("Тип данных / продукт")
+        self.label3.setProperty("class", "label4")
+
+        self.list1 = QComboBox()
+        self.list1.addItem("Банковские карты (bank_cards.xlsx)")
+        self.list1.addItem("Виртуальная тройка (virtual_troika.xlsx)")
+        self.list1.addItem("Мультитранспорт (multitransport.xlsx)")
+        self.list1.addItem("Продажи.xlsx")
+        self.list1.setProperty("class", "QComboBox")
+        self.list1.setCurrentIndex(0)
+        self.list1.update()
+
+        self.label4 = QLabel("Выберите XLSX-файл")
+        self.label4.setProperty("class", "label4")
+
+        self.open_file_loyuot = QHBoxLayout()
+        self.open_file_button = QPushButton("Открыть файл")
+        self.open_file_button.clicked.connect(self.open_file)
+        self.file_name = QLabel("Файл не выбран")
+        self.file_name.setProperty("class", "label4")
+        self.open_file_loyuot.addWidget(self.open_file_button)
+        self.open_file_loyuot.addWidget(self.file_name)
+
+        self.load_data_loyuot.addWidget(self.label1)
+        self.load_data_loyuot.addWidget(self.label2)
+        self.load_data_loyuot.addWidget(self.label3) 
+        self.load_data_loyuot.addWidget(self.list1)
+        self.load_data_loyuot.addWidget(self.label4)
+        self.load_data_loyuot.addLayout(self.open_file_loyuot)
+        self.load_data_loyuot.addStretch()
+
+        self.label5 = QLabel("📑 Параметры отчёта")
+        self.label5.setProperty("class", "label4")
+        self.form_report_loyout.addWidget(self.label5)
+
+        self.load_data.setLayout(self.load_data_loyuot)
+        self.form_report.setLayout(self.form_report_loyout)
 
         self.esc_button = QPushButton("назад")
-        self.esc_button.setProperty("class", "QPushButton")
         self.esc_button.clicked.connect(self.go_to_screen1)
 
-        self.loyuot1.addWidget(self.esc_button)
+        self.main_loyuot.addLayout(self.button_loyout)
+        self.main_loyuot.addWidget(self.stacked_widget)
+        self.main_loyuot.addWidget(self.esc_button)
+        self.main_loyuot.addStretch()
 
-        self.setLayout(self.loyuot1)
+        self.setLayout(self.main_loyuot)
+
+        
+    def show_load_data(self):
+        self.stacked_widget.setCurrentIndex(0)
+
+
+    def show_form_report(self):
+        self.stacked_widget.setCurrentIndex(1)
+
+
+    def open_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Выберите XLSX файл", "", "Excel (*.xlsx)")
+
+        if file_path:
+            self.file = pd.read_excel(file_path)
+            self.file_name.setText(file_path.split("/")[-1])
+
+        # вот тут по идее нужен emit с file а в app вызывать service в котором вызывать db_connect
+
 
     def go_to_screen1(self):
         self.escape.emit()
